57c81e01caa7bee63b3d805e2438309f85a5a5b2
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-08-20 16:32:12 +0200

    - removed nginx from local setup as it is not needed for the app to work - add automated creation of a superuser in the entrypoint of the api-server - updated api-server Dockerfile

---
+++ services/api-server/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python manage.py migrate
+
+echo "from django.contrib.auth.models import User; User.objects.filter(email='${DJANGO_SUPERUSER_EMAIL}').delete(); User.objects.create_superuser('${DJANGO_SUPERUSER_NAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')" | python manage.py shell
+
+python manage.py $@
