b58ed50b4e9133c97238ddaa022f461a005772a9
Jan-Luca Klees <4klees@informatik.uni-hamburg.de>
2017-09-24 10:09:04 +0200

    added cd statement for bash script to work properly

--- services/api-server/docker-entrypoint.sh
+++ services/api-server/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd /kannji/api-server-master/
+
 python manage.py migrate
 
 echo "from django.contrib.auth.models import User; User.objects.filter(email='${DJANGO_SUPERUSER_EMAIL}').delete(); User.objects.create_superuser('${DJANGO_SUPERUSER_NAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')" | python manage.py shell
